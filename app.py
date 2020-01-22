f8322f57e75905a703ccedef56278b7052a92346
Shoaib <41081461+suveb@users.noreply.github.com>
2020-01-23 05:10:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,13 @@ model = pickle.load(open('diabetesmodel.pkl', 'rb'))
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
-def predict():
-    data = request.get_json()
-    p = list(map(float,(f"{data['age']} {data['sex']} {data['bmi']} {data['bp']} {data['s1']} {data['s2']} {data['s3']} {data['s4']} {data['s5']} {data['s6']}").split(' ')))
-    t = np.reshape(p,(1,-1))
-    result = model.predict(t)
-    return jsonify({ 'result': result[0]})
-    
-app.run(debug=True)
+if __name__ == '__main__':
+    @app.route('/',methods=['POST'])
+    def predict():
+        data = request.get_json()
+        p = list(map(float,(f"{data['age']} {data['sex']} {data['bmi']} {data['bp']} {data['s1']} {data['s2']} {data['s3']} {data['s4']} {data['s5']} {data['s6']}").split(' ')))
+        t = np.reshape(p,(1,-1))
+        result = model.predict(t)
+        return jsonify({ 'result': result[0]})
+        
+    app.run(debug=True)
